5f5a0c027c5309af7d8ed43747203421d9a934e0
Victoria <viky_lechka@mail.ru>
2022-09-30 21:07:53 +0300

    Задача 1 про два числа и квадрат

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hellccccco, World!");
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b*b) 
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+
