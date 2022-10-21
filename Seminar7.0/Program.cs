e968943af7f64c244014aa3eeade5699a367c6ee
Victoria <viky_lechka@mail.ru>
2022-10-21 22:06:24 +0300

    twoDimensional array

---
+++ Seminar7.0/Program.cs
@@ -0,0 +1,143 @@
+﻿/* Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. 
+Выведите полученный массив на экран.
+m = 3, n = 4.
+0 1 2 3
+1 2 3 4
+2 3 4 5
+*/
+
+/*
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n];
+
+fillArray(array);
+printArray(array);
+
+int[,] fillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = i + j;
+        }
+    }
+    return arr;
+}
+
+void printArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+*/
+
+/* Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, 
+и замените эти элементы на их квадраты. */
+/* int[,] array = new int[3, 4];
+
+fillArray(array);
+arrayOutput(array);
+square(array);
+arrayOutput(array);
+
+void fillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void square(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j += 2)
+        {
+            matrix[i, j] = matrix[i, j] * matrix[i, j];
+            
+        }
+    }
+}
+
+void arrayOutput(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+           
+
+        }
+        Console.WriteLine(" ");
+    }
+    Console.WriteLine(" ");
+} */
+
+
+/*Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали*/
+
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n];
+
+Console.WriteLine();
+fillArray(array);
+printArray(array);
+Console.WriteLine();
+Console.WriteLine(indexFinder(array));
+
+int[,] fillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return arr;
+}
+
+int indexFinder(int[,] arr)
+{
+    int c = 0;
+    int min = arr.GetLength(0);
+    
+    if (arr.GetLength(0) > arr.GetLength(1))
+    {
+        min = arr.GetLength(1);
+    }
+
+    for (int i = 0; i < min; i++)
+    {
+        c += arr[i, i];
+    }
+    return c;
+}
+
+void printArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
